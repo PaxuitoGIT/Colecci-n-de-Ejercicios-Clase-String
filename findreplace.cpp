e623a9da9ee80abb2001b976fd8ac8e862913484
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-23 10:57:39 +0000

    buscar y reemplazar frase

--- findreplace.cpp
+++ findreplace.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string frase = "Hola gente";
+    size_t pos = frase.find("gente");
+    cout << "Posicion de la palabra 'gente': " << pos << endl;
+    frase.replace(pos, 5, "mundo");
+    cout << "Frase despues del reemplazo: " << frase << endl;
+    return 0;
+}
